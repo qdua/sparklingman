0d474e38f58127938573db3b69a201acdb94097b
Sparklingman <hi@qd.ua>
2021-12-30 11:55:20 +0200

    0.0.12

--- poster/25/app.js
+++ poster/25/app.js
@@ -1,4 +1,4 @@
-let body, box, canvas, land, poster, random, snow
+let body, box, canvas, land, mountain, mountainClone, poster, random, snow
 body = document.body
 poster = document.createElement('div')
 poster.setAttribute('id', 'poster')
@@ -6,6 +6,9 @@ body.prepend(poster)
 canvas = document.createElement('div')
 canvas.setAttribute('id', 'canvas')
 poster.prepend(canvas)
+mountain = document.createElement('div')
+mountain.setAttribute('id', 'mountain')
+canvas.append(mountain)
 land = document.createElement('div')
 land.setAttribute('id', 'land')
 canvas.append(land)
@@ -13,17 +16,20 @@ snow = document.createElement('div')
 snow.setAttribute('id', 'snow')
 canvas.append(snow)
 box = 6
+
+
+
+
 random = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 function colors() {
-    return getComputedStyle(document.documentElement).getPropertyValue('--clr' + random(1, 5) + '')
+    return getComputedStyle(document.documentElement).getPropertyValue('--clr' + random(1, 6) + '')
 }
 for (let i = 0; i < box; i++) {
-    let mountain = document.createElement('span')
-    mountain.style.borderRadius = random(1, 5) + "vmin"
-    canvas.prepend(mountain)
+    let span = document.createElement('span')
+    mountain.prepend(span)
 }
 
 
@@ -33,9 +39,9 @@ function createSnow() {
         let s = document.createElement("s");
         s.style.width = size + "vmin";
         s.style.height = size + "vmin";
-        s.style.left = random(0, 200) + "%";
+        s.style.left = random(0, 160) + "vmin";
         s.classList.add("s");
-        s.style.animationDuration = Math.random() * 3 + 10 + "s";
+        s.style.animationDuration = Math.random() * 5 + 10 + "s";
         s.style.opacity = Math.random();
         snow.prepend(s);
 
@@ -57,3 +63,7 @@ setInterval(createSnow, 1000)
     // setInterval(randomFull, 1000);
 window.requestAnimationFrame(randomFull)
 window.requestAnimationFrame(createSnow)
+
+mountainClone = mountain.cloneNode(true);
+mountainClone.setAttribute('id', 'mountainClone')
+canvas.prepend(mountainClone)
