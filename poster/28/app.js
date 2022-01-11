3c77eda44aac1a850546505bc5b903edaf3d9581
Sparklingman <hi@qd.ua>
2022-01-11 11:07:33 +0200

    0.0.15

---
+++ poster/28/app.js
@@ -0,0 +1,34 @@
+let body, canvas, sea, sun, poster
+body = document.body
+poster = document.createElement('div')
+poster.setAttribute('id', 'poster')
+body.prepend(poster)
+canvas = document.createElement('div')
+canvas.setAttribute('id', 'canvas')
+poster.prepend(canvas)
+
+sea = document.createElement('div')
+sea.setAttribute('id', 'sea')
+canvas.prepend(sea)
+sun = document.createElement('sun')
+sun.setAttribute('id', 'sun')
+canvas.append(sun)
+
+window.setInterval(function() {
+    let span = document.createElement('span')
+    sea.prepend(span)
+    window.setInterval(() => {
+        span.remove()
+    }, 32360)
+}, 2000)
+
+window.setInterval(function() {
+    let span = document.createElement('span')
+    sun.prepend(span)
+    window.setInterval(() => {
+        span.remove()
+    }, 16180)
+
+    let spanL = document.querySelectorAll('#canvas span').length
+    console.log(spanL);
+}, 1000)
